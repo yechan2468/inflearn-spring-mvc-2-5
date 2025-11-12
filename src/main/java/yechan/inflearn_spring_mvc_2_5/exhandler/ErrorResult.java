fbe0828f01351829625b7b2fccb68e2e03827b5b
yechan <yechan24680@gmail.com>
2025-11-12 17:41:33 +0900

    section10 108

---
+++ src/main/java/yechan/inflearn_spring_mvc_2_5/exhandler/ErrorResult.java
@@ -0,0 +1,11 @@
+package yechan.inflearn_spring_mvc_2_5.exhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
